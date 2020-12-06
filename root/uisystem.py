a7b53c62fb02dd6738c0377e9a77ff78efaa9053
Lead0b110010100 <Lead0b110010100@outlook.com>
2020-12-06 10:34:33 +0100

    Fixxed channel changer not opening.

--- root/uisystem.py
+++ root/uisystem.py
@@ -184,3 +184,167 @@ class SystemDialog(ui.ScriptWindow):
 	def OnPressEscapeKey(self):
 		self.Close()
 		return True
+
+########################################### CHANNEL SWITCHER ##########################################################
+class MoveChannelWindow(ui.ScriptWindow):
+	def __init__(self):
+		ui.ScriptWindow.__init__(self)
+		self.titleBar = None
+		ServerStateChecker.Create(self)
+		self.channelButtonList = []
+		self.currentChannel = 0
+		self.__LoadBoard()
+		self.RefreshChannelButtons()
+		self.Show()
+		self.SetCenterPosition()
+		self.RefreshBeforeShow()
+
+	def __del__(self):
+		ui.ScriptWindow.__del__(self)
+
+	def __LoadBoard(self):
+		try:
+			pyScrLoader = ui.PythonScriptLoader()
+			pyScrLoader.LoadScriptFile(self, "UIScript/MoveChannelDialog.py")
+		except:
+			import exception
+			exception.Abort("MoveChannelWindow.__LoadBoard.LoadScript")
+
+		try:
+			self.board = self.GetChild("MoveChannelBoard")
+			self.titleBar = self.GetChild("MoveChannelTitle")
+			self.blackBoard = self.GetChild("BlackBoard")
+			self.okButton = self.GetChild("AcceptButton")
+			self.cancelButton = self.GetChild("CancelButton")
+		except:
+			import exception
+			exception.Abort("MoveChannelWindow.__LoadBoard.BindObject")
+
+		self.titleBar.SetCloseEvent(ui.__mem_func__(self.Close))
+		self.okButton.SetEvent(ui.__mem_func__(self.ChangeChannel))
+		self.cancelButton.SetEvent(ui.__mem_func__(self.Close))
+		self.SetCurentChannel()
+		self.AddChannels()
+		
+	def SetCurentChannel(self):
+		try:
+			self.currentChannel = int( int(net.GetServerInfo().split(",")[1][-1:]) - 1 )
+		except:
+			return
+		
+	def __GetServerID(self):
+		for i in serverInfo.REGION_DICT[0].keys():
+			if serverInfo.REGION_DICT[0][i]["name"] == net.GetServerInfo().split(",")[0]:
+				serverID = int(i)
+				break
+
+		return serverID
+		
+	def __GetChannelNumber(self):
+		serverID = self.__GetServerID()
+		try:
+			channelDict = serverInfo.REGION_DICT[0][serverID]["channel"]
+		except:
+			return
+			
+		return len(channelDict)	
+		
+	def RefreshBeforeShow(self):
+		self.__RequestServerStateList()
+
+	def __RequestServerStateList(self):
+		try:
+			channelDict = serverInfo.REGION_DICT[0][self.__GetServerID()]["channel"]
+		except:
+			return
+		
+		ServerStateChecker.Initialize(self)
+		for id, channelDataDict in channelDict.items():
+			key = channelDataDict["key"]
+			ip = channelDataDict["ip"]
+			udp_port = channelDataDict["udp_port"]
+			ServerStateChecker.AddChannel(key, ip, udp_port)
+		ServerStateChecker.Request()
+
+	def NotifyChannelState(self, addrKey, state, player_count):
+		try:
+			stateName = serverInfo.STATE_DICT[state]
+		except:
+			stateName = serverInfo.STATE_NONE
+		
+		regionID  = int(addrKey / 1000)
+		serverID  = int(addrKey / 10) % 100
+		channelID = addrKey % 10
+		try:
+			serverInfo.REGION_DICT[regionID][serverID]["channel"][channelID]["state"] = stateName
+			self.__RefreshChannelStateList()
+		except:
+			pass
+
+	def AddChannels(self):
+		self.SetSize(190,80+30*self.__GetChannelNumber())
+		self.board.SetSize(190,80+30*self.__GetChannelNumber())
+		self.blackBoard.SetSize(163,7+30*self.__GetChannelNumber())
+
+		for i in xrange(self.__GetChannelNumber()):
+			channelID = self.currentChannel+1
+			self.channelButtonList.append(ui.MakeRadioButton(self.blackBoard, 7, 7+30*i, "","d:/ymir work/ui/game/myshop_deco/", "select_btn_01.sub", "select_btn_02.sub", "select_btn_03.sub"))
+			self.channelButtonList[i].SetText(str(serverInfo.REGION_DICT[0][self.__GetServerID()]["channel"][i+1]["name"]) + " - |cff585858%s" % serverInfo.STATE_LOAD)
+			self.channelButtonList[i].SetEvent(lambda arg=i: self.SelectChannel(arg))
+			self.channelButtonList[i].Show()	
+			
+			
+	def SelectChannel(self,channel):
+		self.currentChannel = channel
+		self.RefreshChannelButtons()
+				
+	def RefreshChannelButtons(self):
+		for i in xrange(self.__GetChannelNumber()):
+			if i == self.currentChannel:
+				self.channelButtonList[i].Down()
+			else:
+				self.channelButtonList[i].SetUp()
+		
+	def ChangeChannel(self):
+		ServerStateChecker.Update()
+		channelID = self.currentChannel+1
+		channelState = serverInfo.REGION_DICT[0][self.__GetServerID()]["channel"][channelID]["state"]
+
+		serverName = serverInfo.REGION_DICT[0][self.__GetServerID()]["name"]
+		channelName = serverInfo.REGION_DICT[0][self.__GetServerID()]["channel"][channelID]["name"]
+
+		if not channelID:
+			return
+		
+		if channelState == serverInfo.STATE_NONE or channelState == serverInfo.STATE_DICT[0]:
+			chat.AppendChat(chat.CHAT_TYPE_INFO , localeInfo.CHANNEL_OFFLINE)
+			return
+			
+		self.Close()
+		net.SendChatPacket("/change_channel " + str(channelID))
+		self.__SetServerInfo("%s, CH%s " % (serverName, channelID))
+
+	def __SetServerInfo(self, name):
+		net.SetServerInfo(name.strip())
+		#self.serverInfo.SetText(name)
+
+	def Destroy(self):
+		self.ClearDictionary()
+		self.Hide()
+		
+	def Close(self):
+		ServerStateChecker.Initialize(self)
+		self.Hide()
+		
+	def OnUpdate(self):
+		ServerStateChecker.Update()
+		for i in xrange(self.__GetChannelNumber()):
+			channelState = serverInfo.REGION_DICT[0][self.__GetServerID()]["channel"][i+1]["state"]
+			if channelState == serverInfo.STATE_NONE:
+				self.channelButtonList[i].SetText(str(serverInfo.REGION_DICT[0][self.__GetServerID()]["channel"][i+1]["name"]) + "   -  |cff585858%s" % serverInfo.REGION_DICT[0][self.__GetServerID()]["channel"][i+1]["state"])
+			else:
+				self.channelButtonList[i].SetText(str(serverInfo.REGION_DICT[0][self.__GetServerID()]["channel"][i+1]["name"]) + " - |cff8099CC%s" % serverInfo.REGION_DICT[0][self.__GetServerID()]["channel"][i+1]["state"])
+
+	def OnPressEscapeKey(self):
+		self.Close()
+		return True
